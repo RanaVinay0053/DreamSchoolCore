b1dbec63125d0718b9553fbcb480a82157ca1075
RanaVinay0053 <prataprana0053@gmail.com>
2020-10-09 19:52:28 +0530

    carousel on home page and dynamic notices

---
+++ DreamSchool.BO/DomainModels/MasterWebsite/NoticeBO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DreamSchool.BO.DomainModels.MasterWebsite
+{
+ public   class NoticeBO
+    {
+        public int Id { get; set; }
+        public string NoticeName { get; set; }
+        public string NoticeDescription { get; set; }
+        public string NoticeDate { get; set; }
+        public string CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string LastModifiedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+       
+    }
+}
